6fb71ee9aa968f23c582d14b4485c376c5446a5b
simple <1669755386@qq.com>
2021-05-21 23:11:47 +0800

    first commit

---
+++ src/main/java/com/atguigu/gitdemo/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.gitdemo;
+
+/**
+ * @Author : ZJJ
+ * @Date : 2021-05-21 22:44
+ * @Description :
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+
+}
