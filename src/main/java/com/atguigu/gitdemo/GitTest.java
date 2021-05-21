ca06d4fd1d856903701e087a16715944b93c5461
simple <1669755386@qq.com>
2021-05-21 23:26:40 +0800

    hot-fix commit-1

--- src/main/java/com/atguigu/gitdemo/GitTest.java
+++ src/main/java/com/atguigu/gitdemo/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test2");
+        System.out.println("git master");
     }
 
 }
